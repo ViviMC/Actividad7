80d3d886db742e78b8722241aa422c15b4cbc935
Viviana Muñoz <AuxiliarAdm@AAASEGUROS.LOCAL>
2021-03-16 20:08:06 -0500

    Carga de Actividad 7

---
+++ js/jquery.js
@@ -0,0 +1,108 @@
+$(".li1").click(function(){
+    $(this).css("color","red")
+})
+
+$(".li1").dblclick(function(){
+    $(this).css("color","black")
+})
+
+$(".li2").click(function(){
+    $(this).css("font-size","36px")
+})
+
+$(".li2").dblclick(function(){
+    $(this).css("font-size","24px")
+})
+
+
+$(".li3").click(function(){
+    $(this).css("background","yellow")
+})
+
+$(".li3").dblclick(function(){
+    $(this).css("background","white")
+})
+
+$(".li4").mouseover(function(){
+    $(this).css("font-family","Segoe Print")
+})
+
+$(".li4").mouseout(function(){
+    $(this).css("font-family","arial")
+})
+
+$(".li5").click(function(){
+    $(this).css("color","violet")
+})
+
+$(".li5").dblclick(function(){
+    $(this).css("background","black")
+})
+
+$(".li6").mouseover(function(){
+    $(this).css("font-size","40px")
+})
+
+$(".li6").mouseout(function(){
+    $(this).css("font-size","24px")
+})
+
+$(".li7").click(function(){
+    $(this).css("text-align","justify")
+})
+
+$(".li7").dblclick(function(){
+    $(this).css("text-align","center")
+})
+
+$(".li8").click(function(){
+    $(this).css("color","blue")
+})
+
+$(".li8").dblclick(function(){
+    $(this).css("color","black")
+})
+
+$(".li9").mouseover(function(){
+    $(this).css("font-family","Freestyle Script")
+})
+
+$(".li9").mouseout(function(){
+    $(this).css("font-family","arial")
+})
+
+$(".li10").click(function(){
+    $(this).css("background","brown")
+})
+
+$(".li10").dblclick(function(){
+    $(this).css("background","green")
+})
+
+
+
+$(".parrafotitulo1").mouseover(function(){
+    $(this).css("background","rgb(118, 171, 250)")
+})
+
+$(".parrafotitulo1").mouseout(function(){
+    $(this).css("background","white")
+})
+
+
+$(".lista").mouseover(function(){
+    $(this).css("background","rgb(118, 171, 250)")
+})
+
+$(".lista").mouseout(function(){
+    $(this).css("background","white")
+})
+
+
+$(".titulo2").mouseover(function(){
+    $(this).css("background","rgb(12, 83, 189)")
+})
+
+$(".titulo2").mouseout(function(){
+    $(this).css("background","white")
+})
